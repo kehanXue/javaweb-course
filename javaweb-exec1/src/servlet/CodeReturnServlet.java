8fe05f985e8bf838c80d272886d6adedcbabafee
Kehan.Xue <kehan.xue@gmail.com>
2019-09-26 18:24:20 +0800

    Finish exec1 step1

---
+++ javaweb-exec1/src/servlet/CodeReturnServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+@WebServlet(name = "CodeReturnServlet")
+public class CodeReturnServlet extends HttpServlet{
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("application/jar");
+        ServletContext context = getServletContext();
+        InputStream inputStream = context.getResourceAsStream("/WEB-INF/classes/bookCode.jar");
+
+        int read = 0;
+        byte[] bytes = new byte[1024];
+        //
+        OutputStream outputStream = response.getOutputStream();
+        while ((read = inputStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, read);
+        }
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doGet(request, response);
+    }
+
+}
